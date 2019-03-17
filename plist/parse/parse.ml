874198bc8e52f2bdabc2beac79f292735501d774
Louis Chan <louischan0325@gmail.com>
2019-03-17 23:15:04 +0800

    Stringify lex error

--- plist/parse/parse.ml
+++ plist/parse/parse.ml
@@ -5,10 +5,25 @@ exception ParseError of string * Lexing.position * Lexing.position
 let initial_position =
   {Lexing.pos_fname= "<filename>"; pos_lnum= 1; pos_bol= 0; pos_cnum= 0}
 
+let string_of_exn exn =
+  let open Lex in
+  match exn with
+  | UnterminatedBlockComment -> "unterminated block comment"
+  | UnterminatedStringLiteral -> "unterminated string literal"
+  | UnterminatedBytes -> "unterminated bytes"
+  | InvalidEscapeSequence -> "invalid escape sequence"
+  | InvalidCharacter ch -> "invalid character `" ^ String.make 1 ch ^ "'"
+  | _ -> "unknown error"
+
 let rec loop lex lexbuf checkpoint =
   match checkpoint with
   | I.InputNeeded _ ->
-      let result = lex lexbuf in
+      let result =
+        try lex lexbuf with e ->
+          let start = lexbuf.Lexing.lex_start_p in
+          let end_ = lexbuf.Lexing.lex_curr_p in
+          raise @@ ParseError (string_of_exn e, start, end_)
+      in
       let checkpoint = I.offer checkpoint result in
       loop lex lexbuf checkpoint
   | I.Shifting _ | I.AboutToReduce _ -> loop lex lexbuf (I.resume checkpoint)
