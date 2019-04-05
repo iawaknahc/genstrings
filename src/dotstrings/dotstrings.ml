1ff0e97173db42d222116e69050172e1b695c7e5
Louis Chan <louischan0325@gmail.com>
2019-04-05 23:44:40 +0800

    Print quoted string properly

--- src/dotstrings/dotstrings.ml
+++ src/dotstrings/dotstrings.ml
@@ -46,18 +46,60 @@ let parse_string s =
   let checkpoint = Dotstrings_parse.Incremental.plist initial_position in
   loop lex lexbuf checkpoint
 
-(* TODO quote *)
-let quote s = s
+let is_printable ch =
+  match Uucp.Gc.general_category ch with
+  | `Ll | `Lm | `Lo | `Lt | `Lu | `Mc | `Me | `Mn | `Nd | `Nl | `No | `Pc
+   |`Pd | `Pe | `Pf | `Pi | `Po | `Ps | `Sc | `Sk | `Sm | `So ->
+      true
+  | _ -> false
+
+let encode int =
+  let i = int - 0x10000 in
+  (0xd800 + ((i lsr 10) land 0x3ff), 0xdc00 + (i land 0x3ff))
+
+let write_quoted_string ch s =
+  output_char ch '"' ;
+  let src = `String s in
+  let encoding = `UTF_8 in
+  let decoder = Uutf.decoder ~encoding src in
+  let buf = Buffer.create 17 in
+  let rec loop () =
+    match Uutf.decode decoder with
+    | `Uchar ch ->
+        let int = Uchar.to_int ch in
+        ( match int with
+        | 0x5c (* \ *) -> Buffer.add_string buf "\\\\"
+        | 0x07 (* bell *) -> Buffer.add_string buf "\\a"
+        | 0x08 (* backspace *) -> Buffer.add_string buf "\\b"
+        | 0x0c (* form feed *) -> Buffer.add_string buf "\\f"
+        | 0x0a (* LF *) -> Buffer.add_string buf "\\n"
+        | 0x0d (* CR *) -> Buffer.add_string buf "\\r"
+        | 0x09 (* tab *) -> Buffer.add_string buf "\\t"
+        | 0x0b (* vertical tab *) -> Buffer.add_string buf "\\v"
+        | 0x22 (* double quote *) -> Buffer.add_string buf "\\\""
+        | 0x20 (* space *) -> Buffer.add_string buf " "
+        | _ ->
+            if is_printable ch then Buffer.add_utf_8_uchar buf ch
+            else if int < 0x10000 then
+              Buffer.add_string buf @@ Printf.sprintf "\\U%04x" int
+            else
+              let r1, r2 = encode int in
+              Buffer.add_string buf @@ Printf.sprintf "\\U%04x\\U%04x" r1 r2 ) ;
+        loop ()
+    | _ -> ()
+  in
+  loop () ;
+  output_bytes ch (Buffer.to_bytes buf) ;
+  output_char ch '"'
 
 let write_channel ch t =
   List.iter
     (fun entry ->
       output_string ch "/*" ;
-      output_string ch entry.comment ;
+      output_string ch (" " ^ String.trim entry.comment ^ " ") ;
       output_string ch "*/\n" ;
-      let key = quote entry.key in
-      output_string ch key ;
+      write_quoted_string ch entry.key ;
       output_string ch " = " ;
-      let value = quote entry.value in
-      output_string ch value ; output_string ch ";\n\n" )
+      write_quoted_string ch entry.value ;
+      output_string ch ";\n\n" )
     t
